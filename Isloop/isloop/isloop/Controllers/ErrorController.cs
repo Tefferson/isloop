b21ba314e77396d45bbe54bbcde24911236ab989
Tefferson <teffersonguterres@gmail.com>
2016-04-20 03:32:37 -0300

    Adicionar páginas de erro

---
+++ Isloop/isloop/isloop/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace isloop.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 200;
+            return View("404");
+        }
+
+        public ActionResult InternalServerError()
+        {
+            Response.StatusCode = 200;
+            return View("500");
+        }
+    }
+}
