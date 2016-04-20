1777a418bc117e9588d41c779f6678b557436744
Tefferson <teffersonguterres@gmail.com>
2016-04-20 03:02:04 -0300

    continue

---
+++ Isloop/isloop/isloop/Domain/Validator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Validator
+    {
+        public IList<Rule> Rules { get; set; }
+
+        public Validator()
+        { }
+
+        public string Validate()
+        {
+            string nl = Environment.NewLine;
+            StringBuilder sb = new StringBuilder();
+            foreach (var rule in Rules)
+            {
+                sb.Append(rule.Id);
+                sb.Append(nl);
+                foreach (var sentence in rule.sentences)
+                {
+                    sb.Append("&nbsp;&nbsp;&nbsp;");
+                    sb.Append(sentence.Id);
+                    if (sentence.Validate()) sb.Append(" - OK");
+                    else sb.Append(" - LOOP");
+                    sb.Append(nl);
+                }
+                sb.Append(nl);
+            }
+            return sb.ToString();
+        }
+    }
+}
